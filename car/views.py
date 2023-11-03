83eae402cce1c861c84a4e3cab4b95bc7f0e4809
Francisco Reyes <juan271292@hotmail.com>
2023-11-03 10:13:46 -0600

    Se agrega djangorestframework-xml

--- car/views.py
+++ car/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
+from rest_framework_xml.renderers import XMLRenderer
 
 from car.models import Car
 from car.serializers import CarSerializer
@@ -7,5 +8,7 @@ from car.serializers import CarSerializer
 
 # Create your views here.
 class CarViewSet(viewsets.ModelViewSet):
+    
+    renderer_classes = [JSONRenderer, XMLRenderer, ]
     queryset = Car.objects.all()
     serializer_class = CarSerializer
